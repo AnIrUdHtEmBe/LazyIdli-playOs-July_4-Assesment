118cdf7f45af11bd95bd428df476a5f868460261
BHAV0207 <bhavyajain4274@gmail.com>
2025-05-14 09:36:02 +0530

    changes

--- src/AssessmentPageComponents/AssignmentCreationModal.tsx
+++ src/AssessmentPageComponents/AssignmentCreationModal.tsx
@@ -1,6 +1,6 @@
-import React, { useContext } from 'react';
-import { DataContext } from '../store/DataContext';
-import './AssignmentCreationModal.css'; // import the CSS file
+import React, { useContext } from "react";
+import { DataContext } from "../store/DataContext";
+import "./AssignmentCreationModal.css"; // import the CSS file
 
 function AssignmentCreationModal() {
   const context = useContext(DataContext);
@@ -15,12 +15,13 @@ function AssignmentCreationModal() {
     <div className="assignment-modal">
       <div className="modal-title">Create Assessment</div>
       <div className="modal-description">
-        Create a personalised assessment to analyze Customers' Goals and recommend a tailored fitness plan
+        Create a personalized assessment to analyze customer goals and recommend
+        a tailored fitness plan.
       </div>
       <div>
         <button
           className="modal-button"
-          onClick={() => setSelectComponent('AssessmentCreationPage2')}
+          onClick={() => setSelectComponent("AssessmentCreationPage2")}
         >
           Create Assessment
         </button>
